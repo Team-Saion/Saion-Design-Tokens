7a666bd009ffed20f8920ad4cb397f114067d34a
moondev03 <moondev03@gmail.com>
2026-05-30 13:53:54 +0900

    fix

--- scripts/common/create-pr-body.mjs
+++ scripts/common/create-pr-body.mjs
@@ -1,8 +1,14 @@
 import fs from 'node:fs/promises';
 
-const [, , platform, changelogPath, outputPath] = process.argv;
+const [, , changelogPath] = process.argv;
 
 const templatePath = '.github/template/token-pr.md';
+
+if (!changelogPath) {
+  throw new Error(
+    'Usage: node scripts/common/create-pr-body.mjs <changelog-path>',
+  );
+}
 
 const template = await fs.readFile(templatePath, 'utf-8');
 const changelog = await fs.readFile(changelogPath, 'utf-8');
@@ -12,8 +18,4 @@ const content = `${template.trim()}
 ${changelog.trim()}
 `;
 
-if (outputPath) {
-  await fs.writeFile(outputPath, content);
-} else {
-  process.stdout.write(content);
-}
+process.stdout.write(content);
